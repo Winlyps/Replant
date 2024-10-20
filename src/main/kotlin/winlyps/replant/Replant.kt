375f7e164169e42915b38d796d4e542ef74413a8
Winlypse <nts3@o2.pl>
2024-10-20 14:15:42 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/replant/Replant.kt
@@ -0,0 +1,14 @@
+package winlyps.replant
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Replant : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/kotlin/winlyps/replant/ReplantListener.kt
@@ -0,0 +1,4 @@
+package winlyps.replant
+
+class ReplantListener {
+}
